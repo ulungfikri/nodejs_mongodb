bfb18f2af6fbf530068051aa08002bb443eb24b4
ulungfikri <ulung.fikri@gmail.com>
2020-11-23 14:24:33 +0700

    Update

--- app/controllers/article.controller.js
+++ app/controllers/article.controller.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const Article = db.article;
+const Article = db.articles;
 
 // Create and Save a new Article
 exports.create = (req, res) => {
@@ -30,126 +30,114 @@ exports.create = (req, res) => {
     });
 };
 
-
-
-// Retrieve all Article from the database.
+// Retrieve all Articles from the database.
 exports.findAll = (req, res) => {
-	const title = req.query.title;
-	var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+  const title = req.query.title;
+  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
 
-	Article.find(condition)
-	.then(data => {
-		res.send(data);
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while retrieving articles."
-		});
-	});
+  Article.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving articles."
+      });
+    });
 };
 
-
-
-// Retrieve a single Article with an id
+// Find a single Article with an id
 exports.findOne = (req, res) => {
-	const id = req.params.id;
+  const id = req.params.id;
 
-	Article.findById(id)
-	.then(data => {
-		if (!data)
-			res.status(404).send({ message: "Not found Article with id " + id });
-		else res.send(data);
-	})
-	.catch(err => {
-		res
-		.status(500)
-		.send({ message: "Error retrieving Article with id=" + id });
-	});
+  Article.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Article with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Article with id=" + id });
+    });
 };
-
-
 
 // Update a Article by the id in the request
 exports.update = (req, res) => {
-	if (!req.body) {
-		return res.status(400).send({
-			message: "Data to update can not be empty!"
-		});
-	}
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
 
-	const id = req.params.id;
+  const id = req.params.id;
 
-	Article.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-	.then(data => {
-		if (!data) {
-			res.status(404).send({
-				message: `Cannot update Article with id=${id}. Maybe Article was not found!`
-			});
-		} else res.send({ message: "Article was updated successfully." });
-	})
-	.catch(err => {
-		res.status(500).send({
-			message: "Error updating Article with id=" + id
-		});
-	});
+  Article.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Article with id=${id}. Maybe Article was not found!`
+        });
+      } else res.send({ message: "Article was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Article with id=" + id
+      });
+    });
 };
-
-
 
 // Delete a Article with the specified id in the request
 exports.delete = (req, res) => {
-	const id = req.params.id;
+  const id = req.params.id;
 
-	Article.findByIdAndRemove(id)
-	.then(data => {
-		if (!data) {
-			res.status(404).send({
-				message: `Cannot delete Article with id=${id}. Maybe Article was not found!`
-			});
-		} else {
-			res.send({
-				message: "Article was deleted successfully!"
-			});
-		}
-	})
-	.catch(err => {
-		res.status(500).send({
-			message: "Could not delete Article with id=" + id
-		});
-	});
+  Article.findByIdAndRemove(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Article with id=${id}. Maybe Article was not found!`
+        });
+      } else {
+        res.send({
+          message: "Article was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Article with id=" + id
+      });
+    });
 };
 
-
-
-// Delete all Article from the database.
+// Delete all Articles from the database.
 exports.deleteAll = (req, res) => {
-	Article.deleteMany({})
-	.then(data => {
-		res.send({
-			message: `${data.deletedCount} Articles were deleted successfully!`
-		});
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while removing all articles."
-		});
-	});
+  Article.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Articles were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all articles."
+      });
+    });
 };
 
-
-
-// Find all published Article
+// Find all published Articles
 exports.findAllPublished = (req, res) => {
-	Article.find({ published: true })
-	.then(data => {
-		res.send(data);
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while retrieving articles."
-		});
-	});
+  Article.find({ published: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving articles."
+      });
+    });
 };
